da9b00db753f4e85c9eea900967a2ab8d7444b2a
crash <crash@KRASHING>
2020-01-29 12:06:45 +0100

    top hud changed to freefont and added highscore label ontop

--- core/src/com/firstrunner/game/Screens/Hud.java
+++ core/src/com/firstrunner/game/Screens/Hud.java
@@ -1,9 +1,11 @@
 package com.firstrunner.game.Screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -11,6 +13,9 @@ import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.firstrunner.game.Firstrunner;
+import com.firstrunner.game.Helpers.Prefferences;
+
+import static com.firstrunner.game.Globals.FONT_ITALIC;
 
 public class Hud implements Disposable {
     public Stage stage;
@@ -19,6 +24,11 @@ public class Hud implements Disposable {
     private static Integer highScore;
     private static Integer score;
     static Label scoreLabel;
+    static Label highscoreLabel;
+
+    FreeTypeFontGenerator fontGenerator;
+    FreeTypeFontGenerator.FreeTypeFontParameter fontParameter;
+    BitmapFont font;
 
     public Hud(SpriteBatch sb){
 
@@ -31,11 +41,27 @@ public class Hud implements Disposable {
         table.top();
         table.setFillParent(true);
 
-        scoreLabel = new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+
+
+        fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal(FONT_ITALIC));
+        fontParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        fontParameter.size = 11;
+        fontParameter.borderWidth = 3;
+        fontParameter.borderColor = Color.BLACK;
+        fontParameter.color = Color.WHITE;
+        font = fontGenerator.generateFont(fontParameter);
+
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        labelStyle.font = font;
+
+        scoreLabel = new Label(String.format("Your: %d", getScore()), labelStyle);
         scoreLabel.setFontScale(0.8f);
 
+        highscoreLabel = new Label(String.format("Highscore: %d", getHighScore()), labelStyle);
+        highscoreLabel.setFontScale(0.8f);
 
         table.add(scoreLabel).padTop(20f);
+        table.add(highscoreLabel).padTop(20f).padLeft(40f);
 
         stage.addActor(table);
 
@@ -51,10 +77,13 @@ public class Hud implements Disposable {
 
     public static void addScore(int value){
         score += value;
-        scoreLabel.setText(String.format("%06d", score));
+        scoreLabel.setText(String.format("Your: %d", score));
     }
 
     public static Integer getHighScore() {
+        highScore = Prefferences.getPreffsHighScore();
+        if (highScore == null)
+            highScore = 0;
         return highScore;
     }
 
